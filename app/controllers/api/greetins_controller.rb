1c50b19e49370525a0478ab44d776d5594de1c8e
Nii Amanor Djoleto <djoletoamanor@gmail.com>
2022-10-01 01:37:24 +0000

    Set up controller

---
+++ app/controllers/api/greetins_controller.rb
@@ -0,0 +1,8 @@
+class Api::GreetingsController < ApplicationController
+    def index
+      random_greeting = Greeting.all.sample
+      @greet = random_greeting.greeting
+      render json: { greeting: @greet }.to_json
+    end
+  end
+  
